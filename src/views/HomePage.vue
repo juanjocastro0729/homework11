6062ba929896e768391b6d1032378c217447c114
Juanjo Castro Rodriguez <jcastr95@stu.southtexascollege.edu>
2025-11-13 22:26:00 -0600

    Initial commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MainCards from '@/components/MainCards.vue';
+import MainPagination from '@/components/MainPagination.vue';
+
+//import { ref } from 'vue'
+//const title = ref('Hello There ✋')
+</script>
+
+<template>
+  <MainCards></MainCards>
+  <MainPagination></MainPagination>
+</template>
